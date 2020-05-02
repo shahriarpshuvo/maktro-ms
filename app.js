92697f872b0476331d833f958e775e37e2e6ef45
Shahriyar Parvez Shuvo <mr.shahriyarshuvo@gmail.com>
2020-05-03 02:40:07 +0600

    connected to database

---
+++ app.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
+
+mongoose.connect(process.env.DATABASE_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Connected to Database"));
+
+
+const app = express();
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
+app.use(methodOverride("_method"));
+
+
+//Routes
+app.get("/", (req, res) => { res.send('It Worked')});
+
+//Development Server
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, ()=>{console.log(`Application started on port: http://localhost:${PORT}`)});
