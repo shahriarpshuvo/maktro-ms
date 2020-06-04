e9c3a2e0262028855868b465a8e6808508e264a4
Shahriyar Parvez Shuvo <mr.shahriyarshuvo@gmail.com>
2020-06-05 05:10:11 +0600

    realse alpha-0.1

--- controllers/DashboardController.js
+++ controllers/DashboardController.js
@@ -1,4 +1,5 @@
 const Expense = require('../models/Expense');
+const Sale = require('../models/Sale');
 
 const DashboardController = {};
 
@@ -6,9 +7,52 @@ const DashboardController = {};
 DashboardController.read = async (req, res) => {
     let queryString={};
     queryString.overview = 'sales';
+    let lookUpProduct = {
+        from: 'products',
+        localField: 'product',
+        foreignField: '_id',
+        as: 'product',
+    };
+    let lookUpCustomer = {
+        from: 'customers',
+        localField: 'customer',
+        foreignField: '_id',
+        as: 'customer',
+    };
 
+    let sales = Sale.aggregate().lookup(lookUpProduct).lookup(lookUpCustomer).unwind({
+        preserveNullAndEmptyArrays: true,
+        path: '$customer',
+    }).unwind({
+        preserveNullAndEmptyArrays: true,
+        path: '$product',
+    });
 
+    if (req.query.salesType == 'customer' && req.query.salesQuery) {
+        sales = sales.match({'customer.phone': req.query.salesQuery});
+        queryString.salesType = req.query.salesType;
+        queryString.startDate = req.query.salesQuery;
+    }
+    if (req.query.salesType == 'product' && req.query.salesQuery) {
+        sales = sales.match({'product.code': req.query.salesQuery});
+        queryString.salesType = req.query.salesType;
+        queryString.salesQuery = req.query.salesQuery;
+    }
 
+    if(req.query.startDateSales){
+        sales = sales.match({ salesDate: {$gte: new Date(req.query.startDateSales)} });
+        queryString.startDateSales = req.query.startDateSales;
+    }
+    if(req.query.endDateSales){
+        sales = sales.match({ salesDate: {$lt: new Date(req.query.endDateSales)}});
+        queryString.endDateSales = req.query.endDateSales;
+    }
+    sales = await sales.exec();
+    const totalSales = sales.reduce((acc, curr) => acc + curr.amount, 0);
+    const totalPaid = sales.reduce((acc, curr) => acc + curr.paid, 0);
+    const totalDue = totalSales - totalPaid;
+    const salesData = { totalSales, totalPaid, totalDue };
+    
     let expenses = Expense.aggregate().match({});
     let totalExp = 0, expTypes = req.query.expcat;
     if(req.query.startDate){
@@ -26,9 +70,8 @@ DashboardController.read = async (req, res) => {
         queryString.expcat = expTypes;
     }
     if(expTypes) queryString.overview = 'expense';
-    res.render('dashboard/index', {totalExp, queryString});
+    res.render('dashboard/index', {totalExp, queryString, salesData});
 };
-
 
 
 module.exports = DashboardController;
