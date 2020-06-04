ab4b2b6b4180ea8c430afb224e687b764a7bf918
Shahriyar Parvez Shuvo <mr.shahriyarshuvo@gmail.com>
2020-06-04 14:29:09 +0600

    auth & role compelete :raised_hands:

--- middlewares/PermissionHandler.js
+++ middlewares/PermissionHandler.js
@@ -2,7 +2,7 @@ module.exports = (role) => {
     return (req, res, next) => {
         if (req.session.user) {
             const currentUserRole = req.session.user.role;
-            if (!role.includes(currentUserRole)) {
+            if (role.includes(currentUserRole)) {
                 return next();
             }
         }
